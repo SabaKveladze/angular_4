bf5b141a96c69ef116ccc92d9ecf0bc8abe0938e
SabaKveladze <sabaqveladze16@gmail.com>
2024-03-25 19:17:39 +0400

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { SignInComponent } from '../app/sign-in/sign-in.component';
+import { SignUpComponent } from '../app/sign-up/sign-up.component';
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
+  signUpForm: FormGroup;
+  // signUpForm = new FormGroup({
+  //   firstname: new FormControl(''),
+  //   lastname: new FormControl(''),
+  //   email: new FormControl(''),
+  //   mobilenom: new FormControl(''),
+  //   password: new FormControl(''),
+  //   confirmpass: new FormControl(''),
+  //   companyname: new FormControl(''),
+  // });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.signUpForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required],
+      mobilenom: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmpass: ['', Validators.required],
+      companyname: ['', Validators.required],
+    });
   }
 
+  ngOnInit(): void {}
+  onFormSubmit() {
+    console.log(this.signUpForm.value);
+  }
 }
